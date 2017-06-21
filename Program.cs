65d6568c02a0d1d85ecefab6cccb920fa0239d4b
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-21 23:13:17 +0300

    task_DEV-2

--- Program.cs
+++ Program.cs
@@ -8,9 +8,17 @@ namespace Numbers
         {
             for (int n = 0; n <= 100; n++)
             {
-                if (n % 3 == 0)
+                if ((n % 3 == 0) && (n % 5 == 0))
                 {
-                    Console.WriteLine("3 * " + n / 3);
+                    Console.WriteLine("Tutti-Frutti");
+                }
+                else if (n % 3 == 0)
+                {
+                    Console.WriteLine("Tutti");
+                }
+                else if (n % 5 == 0)
+                {
+                    Console.WriteLine("Frutti");
                 }
                 else
                 {
