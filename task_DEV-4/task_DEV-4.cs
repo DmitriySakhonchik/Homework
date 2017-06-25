556ec3b58be92353ba784329e91966b8411a2f22
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-26 01:48:34 +0300

    Add task_DEV-4

---
+++ task_DEV-4/task_DEV-4.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class ExceptionMessage
+    {
+        public static bool ShowMessage(string exceptionType, string message)
+        {
+            Console.Write(exceptionType);
+            Console.WriteLine(message);
+            Console.WriteLine("Please, repeat number entry.\n");
+            return true;
+        }
+    }
+
+    class NondecreasingNumberSequence
+    {
+        public static bool MembershipCheck(int[] numberSequence)
+        {
+            bool check = true;
+            for (int i = 1; i < numberSequence.Length; i++)
+            {
+                if (numberSequence[i] - numberSequence[i - 1] < 0)
+                {
+                    check = false;
+                    break;
+                }
+                else
+                {
+                    check = true;
+                }                    
+            }
+            return check;
+        }
+        public static void ShowResult(bool membershipCheck)
+        {
+            if (membershipCheck == true)
+            {
+                Console.WriteLine("\nThis number sequence is a nondecreasing number sequence.");
+            }
+            else
+            {
+                Console.WriteLine("\nThis number sequence is not a nondecreasing number sequence.");
+            }
+        }
+    }
+
+    class NumberSequenceTool
+    {
+        static void Main()
+        {            
+            bool repeat = false;
+            int[] numberSequence;
+            do
+            {
+                Console.WriteLine("Enter a number sequence, dividing numbers by spaces:");
+                string inputString = Console.ReadLine();
+                string[] stringNumbers = inputString.Split();
+                numberSequence = new int[stringNumbers.Length];
+                try
+                {
+                    for (int i = 0; i < stringNumbers.Length; i++)
+                    {
+                        numberSequence[i] = Int32.Parse(stringNumbers[i]);
+                    }
+                    repeat = false;
+                }                
+                catch (FormatException e)
+                {
+                    repeat = ExceptionMessage.ShowMessage("\nFormatException: ", e.Message);
+                }
+                catch (OverflowException e)
+                {
+                    repeat = ExceptionMessage.ShowMessage("\nOverflowException: ", "Number is too large.");
+                }               
+            } while (repeat == true);
+
+            bool membershipCheck = NondecreasingNumberSequence.MembershipCheck(numberSequence);
+            NondecreasingNumberSequence.ShowResult(membershipCheck);
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
