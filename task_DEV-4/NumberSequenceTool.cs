cd457e01a9e393063e2ee5ce58df45bd84a47eee
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-26 17:27:58 +0300

    Update and rename task_DEV-4.cs to NumberSequenceTool.cs
    Removed classes.

--- task_DEV-4/NumberSequenceTool.cs
+++ task_DEV-4/NumberSequenceTool.cs
@@ -2,49 +2,6 @@
 
 namespace NumberSequence
 {
-    class ExceptionMessage
-    {
-        public bool ShowMessage(string exceptionType, string message)
-        {
-            Console.Write(exceptionType);
-            Console.WriteLine(message);
-            Console.WriteLine("Please, repeat number sequence entry.\n");
-            return true;
-        }
-    }
-
-    class NondecreasingNumberSequence
-    {
-        public bool MembershipCheck(int[] numberSequence)
-        {
-            bool check = true;
-            for (int i = 1; i < numberSequence.Length; i++)
-            {
-                if (numberSequence[i] - numberSequence[i - 1] < 0)
-                {
-                    check = false;
-                    break;
-                }
-                else
-                {
-                    check = true;
-                }                    
-            }
-            return check;
-        }
-        public void ShowResult(bool membershipCheck)
-        {
-            if (membershipCheck == true)
-            {
-                Console.WriteLine("\nThis number sequence is a nondecreasing number sequence.");
-            }
-            else
-            {
-                Console.WriteLine("\nThis number sequence is not a nondecreasing number sequence.");
-            }
-        }
-    }
-
     class NumberSequenceTool
     {
         static void Main()
