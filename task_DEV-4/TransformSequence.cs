02804f45afb58c911547e7fedb0e5f4ca7b193b6
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-26 23:15:34 +0300

    Rename TransformString.cs to TransformSequence.cs
    Added field.

---
+++ task_DEV-4/TransformSequence.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class TransformSequence
+    {
+        private int[] numberSequence;
+
+        public bool ParseString(string[] stringNumbers)
+        {
+            numberSequence = new int[stringNumbers.Length];
+            try
+            {
+                for (int i = 0; i < stringNumbers.Length; i++)
+                {
+                    numberSequence[i] = Int32.Parse(stringNumbers[i]);
+                }
+                return false;
+            }
+            catch (FormatException e)
+            {
+                ExceptionMessage exMessage = new ExceptionMessage();
+                return exMessage.ShowMessage("\nFormatException: ", e.Message);
+            }
+            catch (OverflowException e)
+            {
+                ExceptionMessage exMessage = new ExceptionMessage();
+                return exMessage.ShowMessage("\nOverflowException: ", "Number sequence is too large.");
+            }
+        }
+
+        public int[] ReturnSequence()
+        {
+            return numberSequence;
+        }
+    }
+}
