84385bbcfe1fa9132c51961daebd60f24b18829b
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-26 17:50:57 +0300

    Add new class TransformString.cs

---
+++ task_DEV-4/TransformString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class TransformString
+    {
+        public bool ParseString(string[] stringNumbers, int[] numberSequence)
+        {
+            try
+            {
+                for (int i = 0; i < stringNumbers.Length; i++)
+                {
+                    numberSequence[i] = Int32.Parse(stringNumbers[i]);
+                }
+                return false;
+            }
+            catch (FormatException e)
+            {
+                ExceptionMessage exMessage = new ExceptionMessage();
+                return exMessage.ShowMessage("\nFormatException: ", e.Message);
+            }
+            catch (OverflowException e)
+            {
+                ExceptionMessage exMessage = new ExceptionMessage();
+                return exMessage.ShowMessage("\nOverflowException: ", "Number sequence is too large.");
+            }
+        }
+    }
+}
