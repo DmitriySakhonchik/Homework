b11763a43d6921c6d4b3b08aa1407f6e62e237e4
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-26 19:53:32 +0300

    Update ExceptionMessage.cs
    Added field.

--- task_DEV-4/ExceptionMessage.cs
+++ task_DEV-4/ExceptionMessage.cs
@@ -4,9 +4,10 @@ namespace NumberSequence
 {
     class ExceptionMessage
     {
+        public const string REPEAT_ENTRY = "Please, repeat number entry.\n\n";
+        
         public bool ShowMessage(string exceptionType, string message)
         {
-            const string REPEAT_ENTRY = "Please, repeat number entry.\n\n";
             Console.Write(exceptionType);
             Console.WriteLine(message);
             Console.WriteLine(REPEAT_ENTRY);
