dd4887df77c0412ef2842cad472ee15e4d107d37
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-28 16:21:20 +0300

    Update ExceptionMessage.cs
    bool changed to void.

--- task_DEV-4/ExceptionMessage.cs
+++ task_DEV-4/ExceptionMessage.cs
@@ -6,12 +6,11 @@ namespace NumberSequence
     {
         public const string REPEAT_ENTRY = "Please, repeat number entry.\n\n";
         
-        public bool ShowMessage(string exceptionType, string message)
+        public void ShowMessage(string exceptionType, string message)
         {
             Console.Write(exceptionType);
             Console.WriteLine(message);
             Console.WriteLine(REPEAT_ENTRY);
-            return true;
         }
     }
 }
