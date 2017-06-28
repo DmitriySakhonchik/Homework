09120fba1647f6c3e106b1e0cc881b63d4f690c4
DmitriySakhonchik <mityabebop@gmail.com>
2017-06-28 16:41:53 +0300

    Update NumberSequenceCheckTool.cs
    Added args input from command prompt.

--- task_DEV-4/NumberSequenceCheckTool.cs
+++ task_DEV-4/NumberSequenceCheckTool.cs
@@ -7,17 +7,25 @@ namespace NumberSequence
         public const string ENTER_NUMBER_SEQUENCE = "Enter a number sequence, dividing numbers by spaces:";
         public const string EXIT = "\nPress any key to exit...";
 
-        static void Main()
+        static void Main(string[] args)
         {
             bool repeat = false;
             NondecreasingNumberSequence numSeq;
             do
             {
-                Console.WriteLine(ENTER_NUMBER_SEQUENCE);
-                string inputString = Console.ReadLine();
-                string[] stringNumbers = inputString.Split();
-                numSeq = new NondecreasingNumberSequence();
-                repeat = numSeq.MembershipCheck(stringNumbers);
+                if (args.Length == 0 || repeat == true)
+                {
+                    Console.WriteLine(ENTER_NUMBER_SEQUENCE);
+                    string inputString = Console.ReadLine();
+                    string[] stringNumbers = inputString.Split();
+                    numSeq = new NondecreasingNumberSequence();
+                    repeat = numSeq.MembershipCheck(stringNumbers);
+                }
+                else
+                {
+                    numSeq = new NondecreasingNumberSequence();
+                    repeat = numSeq.MembershipCheck(args);
+                }
             } while (repeat);
 
             numSeq.ShowResult();
