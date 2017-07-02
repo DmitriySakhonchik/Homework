45f0566883e5be7accb90a15e6928806f4aa0b81
DmitriySakhonchik <mityabebop@gmail.com>
2017-07-02 13:52:15 +0300

    Update NumberSequenceCheckTool.cs
    Added new class Input.

--- task_DEV-4/NumberSequenceCheckTool.cs
+++ task_DEV-4/NumberSequenceCheckTool.cs
@@ -4,28 +4,17 @@ namespace NumberSequence
 {
     class NumberSequenceCheckTool
     {
-        public const string ENTER_NUMBER_SEQUENCE = "Enter a number sequence, dividing numbers by spaces:";
         public const string EXIT = "\nPress any key to exit...";
 
         static void Main(string[] args)
         {
             bool repeat = false;
-            NondecreasingNumberSequence numSeq;
+            NondecreasingNumberSequence numSeq;          
             do
             {
-                if (args.Length == 0 || repeat == true)
-                {
-                    Console.WriteLine(ENTER_NUMBER_SEQUENCE);
-                    string inputString = Console.ReadLine();
-                    string[] stringNumbers = inputString.Split();
-                    numSeq = new NondecreasingNumberSequence();
-                    repeat = numSeq.MembershipCheck(stringNumbers);
-                }
-                else
-                {
-                    numSeq = new NondecreasingNumberSequence();
-                    repeat = numSeq.MembershipCheck(args);
-                }
+                Input input = new Input();
+                numSeq = new NondecreasingNumberSequence();
+                repeat = numSeq.MembershipCheck(input.NumberSequence(args, repeat));
             } while (repeat);
 
             numSeq.ShowResult();
