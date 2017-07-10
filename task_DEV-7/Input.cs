ffd2a09b202bd39696680673aaf6a14f57b9e7c5
DmitriySakhonchik <mityabebop@gmail.com>
2017-07-10 18:49:05 +0300

    Update Input.cs

--- task_DEV-7/Input.cs
+++ task_DEV-7/Input.cs
@@ -4,11 +4,18 @@ namespace TriangleAnalysis
 {
     class Input
     {
+        private string[] args;
+
         public const string ENTER_TRIANGLE_SIDES = "Enter lengths of triangle sides (LTS), dividing numbers by spaces:";
         public const string CL_ARGUMENTS_LENGTH = "The number of command-line arguments is not equal to 3.";
         public const string SIDES_LENGTHS_NUMBER = "The number of LTS is not equal to 3.";
 
-        public string[] EnterTriangleSides(string[] args, bool indicator)
+        public Input(string[] args)
+        {
+            this.args = args;
+        }
+
+        public string[] EnterTriangleSides(bool indicator)
         {
             string[] triangleSides;
             if (args.Length == 0 || indicator == true)
@@ -20,6 +27,8 @@ namespace TriangleAnalysis
             {
                 triangleSides = EnterTriangleSidesCommandLine(args);
             }
+            TriangleSidesValidator validator = new TriangleSidesValidator(triangleSides);
+            validator.CheckValidity();
             return triangleSides;
         }
 
