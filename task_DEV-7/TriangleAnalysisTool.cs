26aee55096e555e79d8c72453294c6ea098c8973
DmitriySakhonchik <mityabebop@gmail.com>
2017-07-10 19:26:33 +0300

    Update TriangleAnalysisTool.cs
    Changed try-catch location.

--- task_DEV-7/TriangleAnalysisTool.cs
+++ task_DEV-7/TriangleAnalysisTool.cs
@@ -9,44 +9,10 @@ namespace TriangleAnalysis
 
         static void Main(string[] args)
         {
-            Triangle triangle;
-            bool repeat = false;
-            do
-            {
-                triangle = new Triangle();
-                try
-                {
-                    Input input = new Input(args);
-                    triangle.Sides = input.EnterTriangleSides(repeat);
-                    triangle.DetermineType();
-                    repeat = false;
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("\nFormatException: " + e.Message);
-                    Console.WriteLine(REPEAT_ENTRY);
-                    repeat = true;
-                }
-                catch (OverflowException e)
-                {
-                    Console.WriteLine("\nOverflowException: " + e.Message);
-                    Console.WriteLine(REPEAT_ENTRY);
-                    repeat = true;
-                }
-                catch (NullReferenceException e)
-                {
-                    Console.WriteLine("\nNullReferenceException: " + e.Message);
-                    Console.WriteLine(REPEAT_ENTRY);
-                    repeat = true;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("\nException: " + e.Message);
-                    Console.WriteLine(REPEAT_ENTRY);
-                    repeat = true;
-                }               
-            } while (repeat);
-
+            Input input = new Input(args);
+            Triangle triangle = new Triangle();
+            triangle.Sides = input.EnterTriangleSides();
+            triangle.DetermineType();
             triangle.ShowType();
 
             Console.WriteLine(EXIT);
