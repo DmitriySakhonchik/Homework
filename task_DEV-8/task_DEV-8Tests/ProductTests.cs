ee14121831fb2925847847d6f1231a8c86820c23
DmitriySakhonchik <mityabebop@gmail.com>
2017-08-16 18:56:41 +0300

    Update Solution.sln

--- task_DEV-8/task_DEV-8Tests/ProductTests.cs
+++ task_DEV-8/task_DEV-8Tests/ProductTests.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ProductManagement;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using ProductManagement;
 
 namespace ProductManagementTests
 {
